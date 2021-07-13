f0a6f2179467ac2118cea7b47c4e69fca1289268
SkayPC <worldskaynet@gmail.com>
2021-07-13 21:20:19 +0700

    Final Work

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
   return (
     <ChatEngine
       height='100vh'
-      userName='Admin'
-      userSecret='123123'
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       projectID='508d4077-bbfe-4d3a-841a-52b776c4854b'
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
