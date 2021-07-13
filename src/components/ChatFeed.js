2fd3c239f31a08d451f2cb31c748afcaaa7a0284
SkayPC <worldskaynet@gmail.com>
2021-07-13 20:01:05 +0700

    add image

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -14,13 +14,13 @@ const ChatFeed = (props) => {
     return keys.map((key, index) => {
       const message = messages[key];
       const lastMessageKey = index === 0 ? null : keys[index - 1];
-      const isMessage = userName === message.sender.username;
+      const isMyMessage = userName === message.sender.username;
 
       return (
         <div key={`msg_${index}`} style={{ width: '100%' }}>
           <div className="message-block">
             {
-              isMessage
+              isMyMessage
                 ? <MyMessage message={message} />
                 : <TheirMessage message={message} lastMessage={message[lastMessageKey]} />
             }
@@ -28,8 +28,8 @@ const ChatFeed = (props) => {
           <div className="read-receipts"
             style=
             {{
-              marginRight: isMessage ? '18px' : '0px',
-              marginLeft: isMessage ? '0px' : '68px'
+              marginRight: isMyMessage ? '18px' : '0px',
+              marginLeft: isMyMessage ? '0px' : '68px'
             }}>
             read-receipts
           </div>
--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,26 +1,20 @@
 const MyMessage = ({ message }) => {
-  if (message?.attachments?.length > 0) {
+  if (message.attachments && message.attachments.length > 0) {
     return (
-      <img>
+      <img
         src={message.attachments[0].file}
-        alt="message-attachments"
+        alt="message-attachment"
         className="message-image"
         style={{ float: 'right' }}
-      </img>
-    )
+      />
+    );
   }
-  return (
-    <div className="message"
-      style={{
-        float: 'right',
-        marginRight: '18px',
-        color: 'white',
-        backgroundColor: '#3B2A50',
-      }}>
 
+  return (
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
       {message.text}
     </div>
-  )
-}
+  );
+};
 
 export default MyMessage;
