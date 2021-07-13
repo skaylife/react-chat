c9ea4a39883bfa55cd9bdbc38de44e18cf0a6bf2
SkayPC <worldskaynet@gmail.com>
2021-07-13 21:15:03 +0700

    fix

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,61 +1,50 @@
-import { useState } from "react";
+import { useState } from 'react';
 import axios from 'axios';
+import moduleName from 'module'
 
-const LoginForm = () => {
+const projectID = '508d4077-bbfe-4d3a-841a-52b776c4854b';
+
+const Modal = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const authObject = {
-      'Project-ID': "508d4077-bbfe-4d3a-841a-52b776c4854b",
-      'User-Name': username,
-      'User-Secret': password
-    };
+    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
 
     try {
-      await axios.get('https://api.chatengine.io/chats', { headers: authObject })
+      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
 
-      localStorage.setItem('username', username)
-      localStorage.setItem('password', password)
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
 
       window.location.reload();
-    } catch (error) {
-      // ошибки -> try с новым юзернеймом
-
+      setError('');
+    } catch (err) {
+      setError('Неправильный логин или пароль');
     }
-
-  }
+  };
 
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Dragon Чат</h1>
+        <h1 className="title">Drago Чат</h1>
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="input" placeholder="Ваш логин"
-            required
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="input" placeholder="Ваш пароль"
-            required
-          />
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Логин" required />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Пароль" required />
           <div align="center">
             <button type="submit" className="button">
               <span>Зайти в чат</span>
             </button>
           </div>
+          <h2 className="error">{error}</h2>
         </form>
       </div>
     </div>
-  );
-}
 
-export default LoginForm;
+  );
+};
+
+export default Modal;
